63a73d4082129dda45d9c73ae857c29918b0f326
Erick Arostegui Cunza <scorpius86@gmail.com>
2020-04-04 16:47:59 -0500

    Sesion 07

---
+++ 07 - Microservices/Microservices.Demo.Ocelot/Microservices.Demo.Ocelot.Paymet.API/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Demo.Ocelot.Paymet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet("GetPayments")]
+        public string GetPayments()
+        {
+            return "This Payment service";
+        }
+    }
+}
